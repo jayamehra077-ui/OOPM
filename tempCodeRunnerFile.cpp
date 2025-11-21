5ca31d1934c4fad39e722204d9f94bd28f63efc6
jayamehra077-ui <jayamehra077@gmail.com>
2025-11-21 11:21:34 +0530

    CODE

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,18 +1,26 @@
 //GIVEN A CLASS RECTANGLE WITH  2 ATTRIBUTES LENGTH AND BREATH WITH METHOD TO CALCULATE
 // AREA AND PARAMETER OF RECTANGLE ALSO
 // DEMONSTRATE THE USE  THESE METHOD IN A SAMPLE PROGRAM
+//in the above que create a cuboid class which is the drived class if the rectangle class 
+//the cuboid class should have one member only that is height also write a fun to find
+//the volume of the cuboid 
+//note that bcz of inheritance length & breath is already given
+//what are the changes need to bedone in rectangle class for this new task
 #include<iostream>
 using namespace std;
 class rectangle
 {
     public:
+   //protected:
     int length,breath;
     rectangle()
     {
-        //length=breath=1;
+        cout<<"-----default constructor called-------";
+        length=breath=1;
     }
     rectangle(int x,int y)
     {
+        cout<<"----parameterised constructor called---";
         length=x;
         breath=y;
     }
@@ -28,9 +36,36 @@ class rectangle
          cout<<"parameter of rectangle="<<parameter<<endl;
     }
 };
+
+class cuboid: public rectangle
+{
+ //protected
+ public:
+ int height;
+ cuboid()
+ {
+ height=1;
+ }
+ cuboid(int x,int y,int z):rectangle(length,breath)
+ {
+   // length=x;
+    //breath=y;
+    height=z;
+ }
+ int volume()
+ {
+    int volume=length*breath*height;
+    cout<<"volume of cuboid="<<volume<<endl;
+ 
+ cout<<"\n";
+ }
+};
+
 int main()
 {
-  rectangle r1(5,4);
-  r1.area();
-  r1.parameter();  
+    cuboid c1(2,3,4);
+    c1.volume();
+    //c1.area();
+   // c1.parameter();
+
 }
